9c746688bd12ac4fe14cfb97c9e28393d5bb4b22
Gius <gius.trapasso@gmail.com>
2021-09-22 17:29:24 +0200

    [gius] fix testo

--- lib/const/text.dart
+++ lib/const/text.dart
@@ -1,6 +1,6 @@
 const String ctaBecameTrainer = "Join our trainers' team";
 const String ctaFeedback = "Let us know your thoughts";
-const String onboardingTitle1 = "Improove with your skills";
+const String onboardingTitle1 = "Improove your skills";
 const String onboardingDescription1 =
     "Instead of random general workouts, take courses to learn specific skills";
 const String onboardingTitle2 = "All in one app";
